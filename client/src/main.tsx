6c21af5d8f200ee4032b7baf87349766223a209c
ReflectivEI <ReflectivEI.ai@gmail.com>
2026-01-23 20:27:46 -0800

    🗑️ DELETE: Remove client/src/main.tsx (dead code cleanup)

--- client/src/main.tsx
+++
@@ -1,7 +0,0 @@
-console.info('[build]', Date.now());
-
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
